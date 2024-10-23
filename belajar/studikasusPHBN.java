fc41e010b72e1451eef564d4c43a2ecc23d5c9c6
Revanza Raihan Rizqullah <revanzaraihanrizqullah@gmail.com>
2024-10-23 16:02:20 +0800

    Add files via upload

---
+++ belajar/studikasusPHBN.java
@@ -0,0 +1,8 @@
+import dataSoal.dataPHBN;
+
+public class studikasusPHBN {
+    public static void main(String[] args) {
+        dataPHBN data = new dataPHBN();
+        data.tambahData();
+    }
+}
