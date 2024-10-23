44d179d24a4c4a4a1d1e6ec2085c8fe9fd629596
Revanza Raihan Rizqullah <revanzaraihanrizqullah@gmail.com>
2024-10-23 16:24:11 +0800

    Delete belajar directory

--- belajar/studikasusPHBN.java
+++
@@ -1,8 +0,0 @@
-import dataSoal.dataPHBN;
-
-public class studikasusPHBN {
-    public static void main(String[] args) {
-        dataPHBN data = new dataPHBN();
-        data.tambahData();
-    }
-}
